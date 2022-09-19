5c97492f0885b241bf2f8bd6823beb13144193b4
Jeremy Morrison <jeremy.morrison36@gmail.com>
2022-09-19 14:59:42 -0700

    temp broken keymaps

--- nvim/.config/nvim/lua/jerry/toggleterm.lua
+++ nvim/.config/nvim/lua/jerry/toggleterm.lua
@@ -2,7 +2,7 @@ local toggleterm = require 'toggleterm'
 
 toggleterm.setup({
 	size = vim.o.columns * 0.4,
-	open_mapping = [[<c-\>]],
+	open_mapping = [[<c-/>]],
 	hide_numbers = true,
 	shade_filetypes = {},
 	shade_terminals = true,
@@ -24,12 +24,12 @@ toggleterm.setup({
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', ' <esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+  -- local opts = {noremap = true}
+  -- vim.api.nvim_buf_set_keymap(0, 't', ' <esc>', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
