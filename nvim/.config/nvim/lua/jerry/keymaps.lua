7e56064b9e168880718284634ae2b73d88e354df
Jeremy Morrison <jeremy.morrison36@gmail.com>
2022-09-19 15:00:15 -0700

    update keymaps

--- nvim/.config/nvim/lua/jerry/keymaps.lua
+++ nvim/.config/nvim/lua/jerry/keymaps.lua
@@ -1,8 +1,12 @@
+vim.keymap.set('n', 'yr', '$', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>y', '"+y', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>/', ':nohl<cr>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.format({async=true})<CR>", {noremap = true, silent = true})
+
 vim.keymap.set('n', '<leader>dc', [[:put =strftime('%A, %B %d, %Y')<cr>kJ]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>rc', ':source $MYVIMRC<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ic', ':source $MYVIMRC<cr>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', {})
 vim.keymap.set('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', {})
@@ -10,5 +14,4 @@ vim.keymap.set('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers
 vim.keymap.set('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>', {})
 
 vim.keymap.set("n", "<leader>ti", "<cmd>lua _ipython_toggle()<CR>", {noremap = true, silent = true})
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {noremap = true, silent = true})
 
