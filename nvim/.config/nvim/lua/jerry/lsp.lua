48ba7a422ebaaa8c8a201bdf5bda92287bffb15b
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2022-10-16 13:01:00 -0700

    add sumneko-lua

--- nvim/.config/nvim/lua/jerry/lsp.lua
+++ nvim/.config/nvim/lua/jerry/lsp.lua
@@ -1,10 +1,10 @@
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 local on_attach = function(client, bufnr)
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', '<leader>r', '<cmd> lua vim.lsp.buf.format({async=true})<cr>', {noremap = true, silent = true})
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', '<leader>r', '<cmd> lua vim.lsp.buf.format({async=true})<cr>', { noremap = true, silent = true })
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
@@ -15,10 +15,23 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 end
 
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig')['pyright'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  -- capabilities = capabilities,
+}
 
--- require('nvim-lsp-installer').setup{}
--- require('lspconfig')['pyright'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
---     capabilities = capabilities,
--- }
+require 'lspconfig'.sumneko_lua.setup({
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      format = {
+        defaultConfig = {
+          quote_style = 'single'
+        }
+      }
+    }
+  }
+  -- flags = lsp_flags,
+})
