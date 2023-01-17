a540cdfde4bd0eadfeeaadb94a698f20ad970bfc
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-01-17 11:58:59 -0800

    add variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,11 @@
+set -gx EDITOR nvim
+set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+set -gx NNN_TRASH 1
+set -gx NNN_OPTS "Hda"
+
+alias lg=lazygit
+
 fish_vi_key_bindings
 
 zoxide init fish | source
