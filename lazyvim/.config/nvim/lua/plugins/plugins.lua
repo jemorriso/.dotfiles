be12ef5a2421bde522cac65222ee7888a14d9611
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-04-29 21:32:27 -0700

    clean up null-ls config using table.insert

--- lazyvim/.config/nvim/lua/plugins/plugins.lua
+++ lazyvim/.config/nvim/lua/plugins/plugins.lua
@@ -63,22 +63,12 @@ return {
       shade_terminals = false,
     },
   },
-  -- copy lazyvim config for null-ls and add black
   {
     "jose-elias-alvarez/null-ls.nvim",
-    opts = function()
+    opts = function(_, opts)
       local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.fish_indent,
-          nls.builtins.diagnostics.fish,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt,
-          nls.builtins.diagnostics.flake8,
-          nls.builtins.formatting.black,
-        },
-      }
+      table.insert(opts.sources, nls.builtins.formatting.black)
+      table.insert(opts.sources, nls.builtins.diagnostics.flake8)
     end,
   },
 }
