cac38a5b2be0add0d5df746b859b5bbb5acc9703
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-05-03 17:49:55 -0700

    add hammerspoon init.lua

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,22 @@
+fhs.loadSpoon("SpoonInstall")
+
+--------------------------------
+-- START VIM CONFIG
+--------------------------------
+local VimMode = hs.loadSpoon("VimMode")
+local vim = VimMode:new()
+
+vim
+  :disableForApp('Code')
+  :disableForApp('iTerm2')
+  :disableForApp('PyCharm')
+  :disableForApp('Terminal')
+
+vim:shouldDimScreenInNormalMode(false)
+vim:shouldShowAlertInNormalMode(true)
+vim:setAlertFont("Courier New")
+vim:enterWithSequence('kj')
+vim:enableBetaFeature('block_cursor_overlay')
+--------------------------------
+-- END VIM CONFIG
+--------------------------------
