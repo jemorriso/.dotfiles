aa94eede683dd10697856027b2ace0f3e4d3958a
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-07-06 21:48:01 -0700

    add in-progress wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,13 +16,62 @@ end
 -- config.color_scheme = "Tokyo Night Storm (Gogh)"
 -- config.color_scheme = "Tokyo Night (Gogh)"
 config.color_scheme = "Gruvbox Dark (Gogh)"
+-- config.color_scheme = "Gruvbox (Gogh)"
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 13
+
+config.front_end = "WebGpu"
+
 local act = wezterm.action
+local mux = wezterm.mux
 
 wezterm.on("update-right-status", function(window, pane)
 	window:set_right_status(window:active_workspace())
+end)
+
+wezterm.on("gui-startup", function(cmd)
+	-- allow `wezterm start -- something` to affect what we spawn
+	-- in our initial window
+	local args = {}
+	if cmd then
+		args = cmd.args
+	end
+
+	local tab, pane, window = mux.spawn_window({
+		workspace = "utils",
+		cwd = "~",
+	})
+
+	window:spawn_tab({})
+	window:spawn_tab({})
+	window:spawn_tab({})
+
+	-- -- Set a workspace for coding on a current project
+	-- -- Top pane is for the editor, bottom pane is for the build tool
+	-- local project_dir = wezterm.home_dir .. '/wezterm'
+	-- local tab, build_pane, window = mux.spawn_window {
+	--   workspace = 'coding',
+	--   cwd = project_dir,
+	--   args = args,
+	-- }
+	-- local editor_pane = build_pane:split {
+	--   direction = 'Top',
+	--   size = 0.6,
+	--   cwd = project_dir,
+	-- }
+	-- -- may as well kick off a build in that pane
+	-- build_pane:send_text 'cargo build\n'
+	--
+	-- -- A workspace for interacting with a local machine that
+	-- -- runs some docker containners for home automation
+	-- local tab, pane, window = mux.spawn_window {
+	--   workspace = 'automation',
+	--   args = { 'ssh', 'vault' },
+	-- }
+	--
+	-- -- We want to startup in the coding workspace
+	-- mux.set_active_workspace 'coding'
 end)
 
 config.keys = {
@@ -65,8 +114,6 @@ config.keys = {
 	-- 		},
 	-- 	}),
 	-- },
-	-- -- Create a new workspace with a random name and switch to it
-	-- { key = "i", mods = "CTRL|SHIFT", action = act.SwitchToWorkspace },
 	-- Show the launcher in fuzzy selection mode and have it list all workspaces
 	-- and allow activating one.
 	{
