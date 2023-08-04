fd984fae8b36b702cce627a1c9f4d5c888a9e9aa
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-08-03 18:41:40 -0700

    use dark background

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- vim.opt.background = "light"
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
