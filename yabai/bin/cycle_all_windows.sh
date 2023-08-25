fe3a82ddd4e42870676c35037ea00e623f6902e5
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-08-25 14:52:47 -0700

    add cycle app scripts
    the problem with these is that there is no state so it toggles between the 2 most recent 😢

---
+++ yabai/bin/cycle_all_windows.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+# from https://github.com/AlgusDark/dotfiles_raw/tree/main
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	-f | --focus)
+		if [ "$2" = "prev" ]; then
+			pos=-1
+		else
+			pos=1
+		fi
+		;;
+	esac
+	shift
+done
+
+pos=${pos:-1}
+
+focus="$(
+	yabai -m query --windows |
+		jq -e --argjson pos $pos '(.[] | select(."has-focus")) as {$id, $app}
+      | map( select( ((."is-hidden" or ."is-minimized") | not) ) )
+      | sort_by(.space, .frame.x, .frame.y)
+      | map(.id)
+      | .[(index($id)+($pos))%length]'
+)"
+
+yabai -m window --focus "$focus"
