535d4ed8f38e49056cc5210a3905d37a41b80748
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-08-26 01:06:46 -0700

    improve vim mode

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -6,12 +6,19 @@ hs.loadSpoon("SpoonInstall")
 local VimMode = hs.loadSpoon("VimMode")
 local vim = VimMode:new()
 
-vim:disableForApp("Code"):disableForApp("iTerm2"):disableForApp("PyCharm"):disableForApp("Terminal")
+vim:disableForApp("Code")
+vim:disableForApp("Cursor")
+vim:disableForApp("PyCharm")
+vim:disableForApp("iTerm2")
+vim:disableForApp("Terminal")
+vim:disableForApp("Kitty")
+vim:disableForApp("Wezterm")
 
 vim:shouldDimScreenInNormalMode(false)
 vim:shouldShowAlertInNormalMode(true)
 vim:setAlertFont("Courier New")
-vim:enterWithSequence("kj")
+-- vim:enterWithSequence("kj")
+vim:bindHotKeys({ enter = { { "ctrl", "alt" }, "i" } })
 vim:enableBetaFeature("block_cursor_overlay")
 --------------------------------
 -- END VIM CONFIG
