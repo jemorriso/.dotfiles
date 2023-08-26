db66bafdafacd51f394263c5a5c120aa6670e3d2
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-08-26 01:07:28 -0700

    add hammerspoon switcher that improves on skhd launcher

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -24,6 +24,73 @@ vim:enableBetaFeature("block_cursor_overlay")
 -- END VIM CONFIG
 --------------------------------
 
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
-	hs.alert.show("Hello World!")
+local switcher = require("switcher")
+
+-- ======================================= Switcher
+
+--  function to either open apps or switch through them using switcher
+function openswitch(name)
+	return function()
+		if hs.application.frontmostApplication():name() == name then
+			switcherfunc()
+		else
+			hs.application.launchOrFocus(name)
+		end
+	end
+end
+
+k = hs.hotkey.modal.new({ "ctrl", "shift", "alt" }, "a")
+-- function k:entered()
+-- 	hs.alert("Entered mode")
+-- end
+-- function k:exited()
+-- 	hs.alert("Exited mode")
+-- end
+k:bind("", "escape", function()
+	k:exit()
+end)
+
+k:bind("", "a", function()
+	openswitch("Arc")()
+	k:exit()
+end)
+
+k:bind("", "f", function()
+	openswitch("Finder")()
+	k:exit()
+end)
+
+k:bind("", "k", function()
+	openswitch("kitty")()
+	k:exit()
+end)
+
+k:bind("", "m", function()
+	openswitch("Messages")()
+	k:exit()
+end)
+
+k:bind("", "o", function()
+	openswitch("Obsidian")()
+	k:exit()
+end)
+
+k:bind("", "p", function()
+	openswitch("Preview")()
+	k:exit()
+end)
+
+k:bind("", "s", function()
+	openswitch("Spotify")()
+	k:exit()
+end)
+
+k:bind("", "w", function()
+	openswitch("WezTerm")()
+	k:exit()
+end)
+
+k:bind("", "f", function()
+	openswitch("Firefox")()
+	k:exit()
 end)
