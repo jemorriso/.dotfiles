50b340ad66a9fa3321f0efc778ba122c1b2c24ac
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-10-29 19:46:56 -0700

    add smart-splits

--- lazyvim/.config/nvim/lua/plugins/plugins.lua
+++ lazyvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,5 +1,25 @@
 return {
   {
+    "mrjones2014/smart-splits.nvim",
+    config = function()
+      require("smart-splits").setup({
+        resize_mode = {
+          resize_keys = { "left", "down", "up", "right" },
+        },
+      })
+      -- vim.keymap.set("n", "<C-left>", require("smart-splits").move_cursor_left)
+      -- vim.keymap.set("n", "<C-down>", require("smart-splits").move_cursor_down)
+      -- vim.keymap.set("n", "<C-up>", require("smart-splits").move_cursor_up)
+      -- vim.keymap.set("n", "<C-right>", require("smart-splits").move_cursor_right)
+    end,
+    -- keys = {
+    -- { "n", "<C-left>", require("smart-splits").move_cursor_left },
+    -- { "n", "<C-down>", require("smart-splits").move_cursor_down },
+    -- { "n", "<C-up>", require("smart-splits").move_cursor_up },
+    -- { "n", "<C-right>", require("smart-splits").move_cursor_right },
+    -- },
+  },
+  {
     "zbirenbaum/copilot.lua",
     opts = {
       suggestion = { enabled = true },
