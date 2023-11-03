064cbcb9310e4949c49d69eec9c0582d9863b40c
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-11-02 22:12:47 -0700

    add back hammerspoon app switcher

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -17,13 +17,69 @@ vim:disableForApp("Wezterm")
 vim:shouldDimScreenInNormalMode(false)
 vim:shouldShowAlertInNormalMode(true)
 vim:setAlertFont("Courier New")
--- vim:enterWithSequence("kj")
+-- vim:enterWithSequence("en", 1000)
 vim:bindHotKeys({ enter = { { "ctrl", "alt" }, "escape" } })
 vim:enableBetaFeature("block_cursor_overlay")
 
 --------------------------------
 -- SWITCHERS
 --------------------------------
+
+k = hs.hotkey.modal.new({}, "f19")
+-- function k:entered()
+-- 	hs.alert("Entered mode")
+-- end
+-- function k:exited()
+-- 	hs.alert("Exited mode")
+-- end
+k:bind("", "escape", function()
+	k:exit()
+end)
+
+k:bind("", "a", function()
+	hs.application.launchOrFocus("Arc")
+	k:exit()
+end)
+
+k:bind("", "f", function()
+	hs.application.launchOrFocus("Finder")
+	k:exit()
+end)
+
+k:bind("", "k", function()
+	hs.application.launchOrFocus("kitty")
+	k:exit()
+end)
+
+k:bind("", "l", function()
+	hs.application.launchOrFocus("Ableton Live 11 Suite")
+	k:exit()
+end)
+
+k:bind("", "m", function()
+	hs.application.launchOrFocus("Messages")
+	k:exit()
+end)
+
+k:bind("", "o", function()
+	hs.application.launchOrFocus("Obsidian")
+	k:exit()
+end)
+
+k:bind("", "p", function()
+	hs.application.launchOrFocus("Preview")
+	k:exit()
+end)
+
+k:bind("", "s", function()
+	hs.application.launchOrFocus("Spotify")
+	k:exit()
+end)
+
+k:bind("", "t", function()
+	hs.application.launchOrFocus("Todoist")
+	k:exit()
+end)
 
 -- require("window_cycle")
 -- require("app_window_cycle")
