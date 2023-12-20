3b48bd2df31928e5e5cee6b7e710d2bfb1813bfb
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-12-19 21:07:15 -0800

    add vim slime config

--- kickstart.nvim/.config/nvim/init.lua
+++ kickstart.nvim/.config/nvim/init.lua
@@ -283,6 +283,18 @@ require('lazy').setup {
       },
     },
   },
+
+  {
+    'jpalardy/vim-slime',
+    config = function()
+      vim.g.slime_target = 'tmux'
+
+      local slime_dict = {}
+      slime_dict.socket_name = 'default'
+      slime_dict.target_pane = ':.1'
+      vim.g.slime_default_config = slime_dict
+    end,
+  },
 }
 
 -- [[ Setting options ]]
